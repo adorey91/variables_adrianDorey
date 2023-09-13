13a91965e8d97aea3cd4be0c8aaffcfbcdde502a
Adrian Dorey <144232716+adorey91@users.noreply.github.com>
2023-09-13 13:39:50 -0300

    Initial commit

---
+++ variables_adrianDorey/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variables_adrianDorey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Variables");
+            Console.WriteLine("____________");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+        }
+    }
+}
