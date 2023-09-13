45beaef358b3ce76717de9b5843c0b9d5cbb3199
Adrian Dorey <144232716+adorey91@users.noreply.github.com>
2023-09-13 14:18:08 -0300

    added tag and name

--- variables_adrianDorey/Program.cs
+++ variables_adrianDorey/Program.cs
@@ -19,14 +19,26 @@ namespace variables_adrianDorey
 
 
             // declaring score variable to be an "integer" type
-            int score;
+            int score;   // int score=0 is a shortcut
+            int enemyValue;
+            string realName;
+            string gamerTag;
+            float percentile;
 
             //initializing score
             score = 0;
+            enemyValue = 250;
+            realName = "Adrian Dorey";
+            gamerTag = "adorey91";
+            percentile = 85.543f;
 
-            //hud
-            Console.WriteLine(score);
-
+            //pretend gameplay
+            score = score + enemyValue; // "=" gets the value of
+            // score += enemyValue is the same as above
+            //hud 
+            Console.WriteLine(realName +" (" + gamerTag+")");
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine("Percentile Rank: "+percentile);
 
             Console.WriteLine();
             Console.WriteLine("Press any key to exit");
